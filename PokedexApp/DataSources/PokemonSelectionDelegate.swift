846e9627accc42158f4c0e47fa060fb73d9ba254
Ely A Ndiaye <ely.assumpcao.ndiaye@accenture.com>
2019-09-24 11:06:07 -0300

    Replace das Datasources CollectionView

---
+++ PokedexApp/DataSources/PokemonSelectionDelegate.swift
@@ -0,0 +1,9 @@
+//
+//  PokemonSelectionDelegate.swift
+//  PokedexApp
+//
+//  Created by ely.assumpcao.ndiaye on 24/09/19.
+//  Copyright © 2019 Ely Assumpcao Ndiaye. All rights reserved.
+//
+
+import Foundation
